d5f6df5a57f53639d51af8ca05e2f391b163ed82
Onur Kansoy <onurkansoy01@gmail.com>
2025-06-15 21:33:54 +0300

    Revert "Merge branch 'master' of https://github.com/Teamuchin/AcilOkeyApp"
    This reverts commit 9e071f756b05a07fc3932b9c31851906ca6af955, reversing
    changes made to 64a07ec438a6ea15e97fa8682accc1229fd4ee53.

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import { Button } from '@rneui/themed'; // If using RNElements buttons
 import { Ionicons } from '@expo/vector-icons'; // For icons
 import DropdownButton from './components/DropdownMenu';
 import NotificationModal from './components/NotificationModal';
+import WigglingOkeyTile from './components/WigglingOkeyTile'; // Add this import
 
 type RootStackParamList = {
   MainApp: undefined;
@@ -54,7 +55,7 @@ export default function App() {
             name="MainApp"
             component={BottomTabNavigator}
             options={{ 
-              title: 'Acil Okey',
+              title: '', // Remove title to make room for custom header
               headerStyle: {
                 backgroundColor: '#D90106',
               },
@@ -64,7 +65,7 @@ export default function App() {
                 fontFamily: 'Poppins-Bold',
               },
               headerRight: () => (
-                <View style={headerstyles.container}>
+                <View style={headerstyles.rightContainer}>
                   <NotificationModal/>
                   <DropdownButton/>
                 </View>
--- components/Auth.tsx
+++ components/Auth.tsx
@@ -49,6 +49,7 @@ export default function Auth() {
     })
 
     if (error) Alert.alert(error.message)
+    if (!session) Alert.alert('Please check your inbox for email verification!')
     setLoading(false)
   }
 
